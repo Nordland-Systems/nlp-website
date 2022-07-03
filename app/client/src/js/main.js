32dc1ec3ff113dd763088ab5933a2f8189e54212
Steffen Kahl <stevenpaw209@gmail.com>
2022-07-03 19:01:15 +0200

    [CHANGED] FAQ-Items Arrow for outfolding

--- app/client/src/js/main.js
+++ app/client/src/js/main.js
@@ -95,37 +95,39 @@ function TableOfContents() {
     const textContent = document.querySelector('[data-behaviour="textContent"]');
     const tableofcontents = document.querySelector('[data-behaviour="tableofcontents"]');
 
-    textContent.innerHTML =
-        textContent.innerHTML.replace(
-            /<h([\d])>([^<]+)<\/h([\d])>/gi,
-            function (str, openLevel, titleText, closeLevel) {
-                if (openLevel != closeLevel) {
-                    return str;
+    if(textContent){
+        textContent.innerHTML =
+            textContent.innerHTML.replace(
+                /<h([\d])>([^<]+)<\/h([\d])>/gi,
+                function (str, openLevel, titleText, closeLevel) {
+                    if (openLevel != closeLevel) {
+                        return str;
+                    }
+
+                    if (openLevel > level) {
+                        toc += (new Array(openLevel - level + 1)).join('<ul>');
+                    } else if (openLevel < level) {
+                        toc += (new Array(level - openLevel + 1)).join('</li></ul>');
+                    } else {
+                        toc += (new Array(level+ 1)).join('</li>');
+                    }
+
+                    level = parseInt(openLevel);
+
+                    var anchor = titleText.replace(/ /g, "_");
+                    toc += '<li><a href="'+window.location.href+'#' + anchor + '">' + titleText
+                        + '</a>';
+
+                    return '<h' + openLevel + '><a href="'+window.location.href+'#' + anchor + '" id="' + anchor + '">'
+                        + titleText + '</a></h' + closeLevel + '>';
                 }
+            );
 
-                if (openLevel > level) {
-                    toc += (new Array(openLevel - level + 1)).join('<ul>');
-                } else if (openLevel < level) {
-                    toc += (new Array(level - openLevel + 1)).join('</li></ul>');
-                } else {
-                    toc += (new Array(level+ 1)).join('</li>');
-                }
-
-                level = parseInt(openLevel);
-
-                var anchor = titleText.replace(/ /g, "_");
-                toc += '<li><a href="'+window.location.href+'#' + anchor + '">' + titleText
-                    + '</a>';
-
-                return '<h' + openLevel + '><a href="'+window.location.href+'#' + anchor + '" id="' + anchor + '">'
-                    + titleText + '</a></h' + closeLevel + '>';
-            }
-        );
-
-    if (level) {
-        toc += (new Array(level + 1)).join('</ul>');
+        if (level) {
+            toc += (new Array(level + 1)).join('</ul>');
+        }
+        tableofcontents.innerHTML += toc;
     }
-    tableofcontents.innerHTML += toc;
 };
 
 function parallax() {
